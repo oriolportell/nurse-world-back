a77fd12fa58b08143f90a0337a6b89a21bdd9692
oriol portell <oriol.portellpareras@amdeus.com>
2023-07-27 15:31:46 +0200

    First commit

---
+++ nurse-world-back/nurse-world-jwt/src/main/java/nurse/world/App.java
@@ -0,0 +1,13 @@
+package nurse.world;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
